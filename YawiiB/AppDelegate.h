e28c7e022a436fc17942e5b4fb1a414321ba8f18
AlexZzz <wenqing.alex.zhou@gmail.com>
2012-10-25 11:21:33 +0300

    Initial Commit

---
+++ YawiiB/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  YawiiB
+//
+//  Created by wenqing zhou on 10/25/12.
+//  Copyright (c) 2012 Yawii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ YawiiB/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  YawiiB
+//
+//  Created by wenqing zhou on 10/25/12.
+//  Copyright (c) 2012 Yawii. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
